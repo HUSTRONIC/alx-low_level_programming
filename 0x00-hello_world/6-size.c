f15dae73fbb3e5326e2386779c27072e5df017e6
hustronic <ssesangahussein.sh@gmail.com>
2023-09-07 16:00:10 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main -it prints out output using put
+*
+* Return: always (0)
+*
+*/
+int main(void)
+{
+printf("Size of a char: %d bytes\n", sizeof(char));
+printf("Size of an int: %d bytes\n", sizeof(int));
+printf("Size of a long int: %d bytes\n", sizeof(long int));
+printf("Size of a long long int: %d bytes\n", sizeof(long long int));
+printf("Size of a float: %d bytes\n", sizeof(float));
+return (0);
+}
+
