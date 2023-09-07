8c4a9865c36004e26429de145f9dfb3e3975dd0f
hustronic <ssesangahussein.sh@gmail.com>
2023-09-07 15:32:05 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main -it prints out output using put
+*
+* Return: always (0)
+*
+*/
+int main(void)
+{
+puts("\"with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
