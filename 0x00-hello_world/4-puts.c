3d7cf77aa0d025533f403f0279c2e49b5aac1b44
hustronic <ssesangahussein.sh@gmail.com>
2023-09-07 13:19:13 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main is the starting point 
+*it prints text to screen using puts
+*/
+Int main(void)
+{
+	Puts(“\"Programming is like building a multilingual puzzle”);
+	Return(0);
+}
+
