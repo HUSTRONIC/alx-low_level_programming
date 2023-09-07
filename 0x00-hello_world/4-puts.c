5917885ee89385fd0a45ef81c19a241bc588d550
hustronic <ssesangahussein.sh@gmail.com>
2023-09-07 13:44:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main -it prints out output using put
- *
- * Return: always (0)
- *
- */
+* main -it prints out output using put
+*
+* Return: always (0)
+*
+*/
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle\n");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
+
