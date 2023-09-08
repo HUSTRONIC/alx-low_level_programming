455f36cae258ef0148d6bee6780cfc88c945432a
hustronic <ssesangahussein.sh@gmail.com>
2023-09-07 17:23:25 -0700

    #include <stdio.h>

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main – outputs Dora’s quote
+ *
+ * Description: prints Dora's quote
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	fprint("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
+
