bac5a1ed93bbdd69a6920a0062d0d246f8ff27f7
hustronic <ssesangahussein.sh@gmail.com>
2023-09-08 15:10:26 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 /**
-* main -it prints out output using put
+* main -it prints out output of lower case
 *
 * Return: always (0)
 *
 */
 int main(void)
 {
-    
+	char c;
 
-        char c;
+	char d;
 
-        char d;
-
-        c = 'a';
-        c = 'A';
-        while
-                (c <= 'z') {
-                        putchar(c);
-                        c++;
-                }
-        while
-                (d <= 'z') {
-                        putchar(d);
-                        d++;
-                }
-        putchar('\n');
-        return (0);
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z'') {
+			putchar(c);
+			c++
+		}
+	while
+		(d <= 'z') {
+			putchar (d);
+			d++
+		}
+	putchar('\n');
+	return (0);
 }
-
