3dc7e37e2d513e5d1192d412c2391bbfbc7d4e5f
hustronic <ssesangahussein.sh@gmail.com>
2023-09-08 16:36:35 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main -it prints out output using put
+*
+* Return: always (0)
+*
+*/
+int main(void)
+{
+	int n =97;
+	while (n <=122)
+	{
+		if (n == 101 ||  n == 113)
+		{
+				n++;
+				continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
