a10d67e57e451bd963defd81d11b45ad0a5a601e
hustronic <ssesangahussein.sh@gmail.com>
2023-09-08 13:55:08 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+* main -it prints out output using put
+*
+* Return: always (0)
+*
+*/
+int main(void)
+{
+        char c;
+
+	char d;
+
+        c = 'a';
+	c = 'A';
+        while
+                (c <= 'z') {
+                        putchar(c);
+                        c++;
+                }
+	while
+                (d <= 'z') {
+                        putchar(d);
+                        d++;
+                }
+        putchar('\n');
+        return (0);
+}
