09cc8cebc6fdc3ab7e8a1c85adfdf76c370d5ac3
hustronic <ssesangahussein.sh@gmail.com>
2023-09-09 11:02:29 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,5 +7,28 @@
 */
 int main(void)
 {
+	int i 0;
+	int j;
+
+	while (i <= '9')
+	{
+		j = '0';
+		while (j <= '9')
+		{
+			if (i < j)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != '8' || i == '8' && j != '9')
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	
+	}
+	putchar('\n');
+	return (0);
 
 }
