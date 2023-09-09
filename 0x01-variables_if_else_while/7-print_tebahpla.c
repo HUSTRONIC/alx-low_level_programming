4f99722df34b153254677097f227c3eaf6398922
hustronic <ssesangahussein.sh@gmail.com>
2023-09-08 18:21:39 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main -it prints number in reverse
+*
+* Return: always (0)
+*
+*/
+int main(void)
+{
+	int n = 122;
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	print('\n');
+	return (0);
+}
