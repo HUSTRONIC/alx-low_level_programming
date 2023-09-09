c80e71f4598474ec06514dafa39ff72f4c4f9dde
hustronic <ssesangahussein.sh@gmail.com>
2023-09-08 17:17:41 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main -it prints numbers of base ten
+*
+* Return: always (0)
+*
+*/
+int main(void)
+{
+	int n;
+	for (n =0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return(0);
+
+}
