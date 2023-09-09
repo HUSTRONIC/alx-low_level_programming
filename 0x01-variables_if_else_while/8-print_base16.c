fe4ada31b3291d578878ab2ef423c7187d4ad453
hustronic <ssesangahussein.sh@gmail.com>
2023-09-08 18:48:46 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main -it prints hexagonal numbers in lower case
+*
+* Return: always (0)
+*
+*/
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n >= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0)
+}
