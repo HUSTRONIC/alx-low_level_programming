441381805c3a1fdadba5662c35b911b40c77783f
hustronic <ssesangahussein.sh@gmail.com>
2023-09-10 08:36:14 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,8 +20,8 @@ int main(void)
 				putchar(k + '0');
 				if (i != 7 || (j != 8 && k != 9))
 				{
-						putchar(',');
-						putchar(' ');
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
