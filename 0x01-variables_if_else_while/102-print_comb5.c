2b4f7169e29e4005bd07045746036f7f33281f0e
hustronic <ssesangahussein.sh@gmail.com>
2023-09-10 13:10:56 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,15 +13,18 @@ int main(void)
 	{
 		for (num2 = num1; num2 <= 99; num2++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-			if (num1 != 99 || num2 != 99)
+			if (num1 != num2)
 			{
-				putchar(',');
+				putchar((num1 / 10) + '0');
+				putchar((num1 % 10) + '0');
 				putchar(' ');
+				putchar((num2 / 10) + '0');
+				putchar((num2 % 10) + '0');
+				if (num1 != 99 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 
