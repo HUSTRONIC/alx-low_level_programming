97b1151599d8c89ca8ce0da104db041e689a8aae
hustronic <ssesangahussein.sh@gmail.com>
2023-09-12 16:37:01 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @a: function parameter
+ * Return: -a or a
+ */
+
+int _abs(int)
+{
+	if (a < 0)
+		return (-a);
+	else
+	{
+		return (a);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,7 @@ void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
 
 
 #endif
