166db4fbb74cd5bfac7c7c92d90f8af72d6934a5
hustronic <ssesangahussein.sh@gmail.com>
2023-09-12 12:13:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  a function that prints 10 times the alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+
+}
