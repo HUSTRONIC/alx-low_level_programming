a807dc48de6ba0298fb2df57603773408d659353
hustronic <ssesangahussein.sh@gmail.com>
2023-09-13 11:41:02 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,7 +14,7 @@ int main(void)
 	printf("%lu, ", term1);
 	for (i = 1; i < 50; i++)
 	{
-		printf("%lu, ", term2);
+		printf("%lu", term2);
 		next = term1 + term2;
 		term1 = term2;
 		term2 = next;
