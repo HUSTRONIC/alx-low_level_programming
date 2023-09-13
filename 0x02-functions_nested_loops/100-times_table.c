e70d1b30c31cadf7e0f82d91c50d260b3c611960
hustronic <ssesangahussein.sh@gmail.com>
2023-09-13 09:21:21 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,11 +31,11 @@ void print_times_table(int n)
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar((pro / 10) + '0');
+					_putchar((prod / 10) + '0');
 				}
 				_putchar((prod % 10) + '0');
 			}
-			_putchar('\n);
+			_putchar('\n');
 		}
 	}
 }
