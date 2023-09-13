5f209d71daa6947191184b36f9ada667186b3c27
hustronic <ssesangahussein.sh@gmail.com>
2023-09-13 15:46:58 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -34,7 +34,7 @@ int main(void)
 		}
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
-			printf(",");
+			printf(", ");
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
