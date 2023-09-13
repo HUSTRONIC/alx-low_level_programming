b53ebfc13f92b4bbb2dc20658ce5144000d62bd7
hustronic <ssesangahussein.sh@gmail.com>
2023-09-12 18:03:31 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,10 +14,10 @@ void jack_bauer(void)
 		for (j = 0; j < 60; j++)
 		{
 			_putchar(i / 10 + '0');
-			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 			_putchar(':');
 			_putchar(j / 10 + '0');
-			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 			_putchar('\n');
 		}
 	}
