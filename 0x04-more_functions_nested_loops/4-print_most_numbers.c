0ca34915d4e55d1521ae2884344509273ec82a79
hustronic <ssesangahussein.sh@gmail.com>
2023-09-14 16:50:56 -0700

    net

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers  - function that prints the numbers, from 0 to 9
+ * print_most_numbers  - function that prints the numbers, from 0 to 9
  * Return: void
  */
 
