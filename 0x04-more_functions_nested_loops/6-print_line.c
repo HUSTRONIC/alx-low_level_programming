f379802efc843ea4b0d5f818322716caf4ab7470
hustronic <ssesangahussein.sh@gmail.com>
2023-09-14 17:27:13 -0700

    6-main.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line in the terminal
+ * @n: input integer
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	for (i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
