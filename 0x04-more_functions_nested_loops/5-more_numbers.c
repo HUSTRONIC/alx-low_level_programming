36d44b93159ab19f8aa07a23c15fb6ff0e93c13d
hustronic <ssesangahussein.sh@gmail.com>
2023-09-14 17:13:21 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,9 +16,9 @@ void more_numbers(void)
 		{
 			if (j >= 10)
 			{
-				_putchar((j / 10 ) + '0');
+				_putchar((j / 10) + '0');
 			}
-			_putchar((j % 10 ) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
