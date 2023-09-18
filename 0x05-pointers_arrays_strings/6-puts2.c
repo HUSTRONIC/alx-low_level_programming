4c96ed835727ef3904ae9b0308e89f802b2a4b7b
hustronic <ssesangahussein.sh@gmail.com>
2023-09-18 14:04:28 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *puts2 - print every other character.
+ *@str: string argument.
+ */
+
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		if (len % 2 == 0)
+		{
+			_putchar(str[len]);
+		}
+		len++;
+	}
+	_putchar('\n');
+}
