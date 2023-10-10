6121c41fed40e2911d2837a11cc525b25cb033ba
hustronic <ssesangahussein.sh@gmail.com>
2023-10-10 15:32:46 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - program that prints the name of the file 
+  * main - program that prints the name of the file
   * it was compiled from, followed by a new line.
   *
   * Return: 0
