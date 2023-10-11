819c52268b54139bc997305b701c400adcd6f789
hustronic <ssesangahussein.sh@gmail.com>
2023-10-11 05:59:46 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_DOG
-#define _HEADER_DOG
+#ifndef _HEADER_DOG_H
+#define _HEADER_DOG_H
 
 /**
   * struct dog - Dog attributes
@@ -25,4 +25,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
 #endif
