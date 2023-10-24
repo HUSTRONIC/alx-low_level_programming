b7545a074ac4e1781eb585e9231b5798770306bc
HUSTRONIC <ssesangahussein.sh@gmail.com>
2023-10-24 11:38:08 -0700

    102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - function that frees a list
+ * @h: pointer that points the node
+ *
+ * Return: The required number of elements.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t m = 0;
+	int z;
+	listint_t *w;
+
+	if (!h || !*h)
+	return (0);
+
+	while (*h)
+	{
+		z = *h - (*h)->next;
+		if (z > 0)
+		{
+		w = (*h)->next;
+		free(*h);
+		*h = w;
+		m++;
+		}
+		else
+		{
+		free(*h);
+		*h = NULL;
+ 		m++;
+	 break;
+		}
+	}
+	*h = NULL;
+
+	return (m);
+}
